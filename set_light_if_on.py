34a49b9ea1881eeb934ca2fb224fe65ebbfee0f9
Sam Steele <sam.steele@gmail.com>
2021-02-24 22:02:15 -0500

    Handle comma seperated list for entity_id

--- set_light_if_on.py
+++ set_light_if_on.py
@@ -5,6 +5,9 @@ def set_entity(entity_id, light_data):
 		hass.services.call("light", "turn_on", light_data, False)
 
 entity_id = data.get("entity_id")
+if "," in entity_id:
+    entity_id = entity_id.split(",")
+
 light_data = {}
 for k, v in data.items():
 	light_data[k] = v
@@ -13,4 +16,4 @@ if isinstance(entity_id, str):
 	set_entity(entity_id, light_data)
 else:
 	for entity in entity_id:
-		set_entity(entity, light_data)
+		set_entity(entity.strip(), light_data)
